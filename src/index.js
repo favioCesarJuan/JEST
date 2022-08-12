8c9bf2b1b3555676718ea3c2ce0976b6b9bc0faf
Favio <faviocjuan@gmail.com>
2022-08-12 08:00:10 -0300

    First test

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const sumar = (a, b) => a + b
+
+const restar = (a, b) => a - b
+
+const multiplicar = (a, b) => a * b
+
+const dividir = (a, b) => a / b;
+
+export { sumar, restar, multiplicar, dividir }
